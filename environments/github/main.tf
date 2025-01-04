e98538dacf8e53f327ea1f029569e4e4a63eebb1
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2025-01-04 18:57:21 +0000

    add base github repositories infra

---
+++ environments/github/main.tf
@@ -0,0 +1,27 @@
+module "infra" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.18.0"
+
+  name        = "infra"
+  description = "Infrastructure to be used by my DevOps studies"
+  visibility  = "public"
+}
+
+module "python-app" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.18.0"
+
+  name        = "python-app"
+  description = "A simple Python application to be used by my DevOps studies"
+  visibility  = "public"
+}
+
+module "node-app" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.18.0"
+
+  name        = "node-app"
+  description = "A simple Node.js application to be used by my DevOps studies"
+  visibility  = "public"
+}
+
