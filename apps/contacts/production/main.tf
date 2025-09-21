86460c12fcabb9699c1ddce9a1bf41b041639e30
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2025-09-21 17:54:14 +0000

    somethings

--- apps/contacts/production/main.tf
+++ apps/contacts/production/main.tf
@@ -7,6 +7,26 @@ module "contacts" {
   version     = "~> 0.18.0"
   visibility  = "public"
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name = "${local.environment}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  map_public_ip_on_launch = true
+  single_nat_gateway      = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
 module "eks" {
   eks_managed_node_groups = {
     main = {
